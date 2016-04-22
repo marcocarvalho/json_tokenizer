17ccfb2195d66f60e9305f78019a56f632b6e3e7
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2016-04-22 11:45:18 -0300

    Custom Types ok

--- pascal_custom_type.rb
+++ pascal_custom_type.rb
@@ -1,16 +1,23 @@
 require './pascal_base'
 
 class PascalCustomType < PascalBase
-
   def arrays
-    array(name, declaration)
+    @arrays ||=
+      array(name, declaration)
+        .flatten
+        .compact
+        .each_with_object({}) do |item, hash|
+          hash.merge!(item)
+        end
   end
 
   def array(name, hash)
     return nil unless hash.is_a?(Hash)
     hash.flat_map do |key, value|
-      if key == :array
-        "  #{array_type(name)} = array of #{t_name(name)}"
+      if key == :array && value.is_a?(Hash)
+        [ { array_type(name) => t_name(name) }, array( name, value) ]
+      elsif key == :array && !value.is_a?(Hash)
+        [ array_type(name) => value ]
       else
         array(key, value)
       end
@@ -18,12 +25,8 @@ class PascalCustomType < PascalBase
   end
 
   def template
-    declaration.select { |_, value| value.is_a?(Hash) }.map do |key, value|
-      if(value.values.first.is_a?(String))
-        "  #{array_type(key)} = array of #{value.values.first};"
-      else
-        "  #{array_type(key)} = array of #{t_name(key)};"
-      end
+    arrays.map do |key, value|
+      "  #{key} = array of #{value};"
     end
   end
 end
